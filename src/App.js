04868348f61a90eb641d4aca3ad3ad9a9f973d2f
JasonNelemans <jasonnelemans@hotmail.com>
2020-02-29 14:11:45 +0100

    Linked profile and home with Link and Switch components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Switch>
+        <Route path="/profile" />
+        <Route path="/" component={Main} />
+      </Switch>
       <Main />
       <Footer />
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div>
       Navbar
-      <button>Home</button>
-      <button>Profile</button>
+      <Link to="/profile"><button>Profile</button></Link>
+      <Link to="/"><button>Home</button></Link>
     </div>
   )
 }
